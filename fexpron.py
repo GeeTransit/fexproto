8025945f93fb06232321897535330237216a488f
George Zhang <geetransit@gmail.com>
2024-08-11 02:27:54 -0400

    Error on unknown expression types just in case

--- fexpron.py
+++ fexpron.py
@@ -3,6 +3,8 @@ def f_eval(env, expr):
         return env[expr]
     elif type(expr) is tuple:
         return env[expr[0]](env, expr[1])
+    else:
+        exit(f'unknown expression type: {expr}')
 
 _DEFAULT_ENV = {
     "+": lambda env, expr: expr[0] + expr[1][0]
